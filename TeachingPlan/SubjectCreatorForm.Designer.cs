513364cb6da7c252b86d079e79ff2f07eb923dfd
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-02 14:20:55 +0200

    Create new form

---
+++ TeachingPlan/SubjectCreatorForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TeachingPlan
+{
+    partial class SubjectCreatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subjectsGridView = new System.Windows.Forms.DataGridView();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // subjectsGridView
+            // 
+            this.subjectsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.subjectsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.subjectsGridView.Location = new System.Drawing.Point(30, 30);
+            this.subjectsGridView.Name = "subjectsGridView";
+            this.subjectsGridView.RowTemplate.Height = 33;
+            this.subjectsGridView.Size = new System.Drawing.Size(2140, 550);
+            this.subjectsGridView.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exitButton.Location = new System.Drawing.Point(2035, 620);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(135, 61);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Wyjdź";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.saveButton.Location = new System.Drawing.Point(1798, 620);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(217, 61);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Zapisz i wyjdź";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // SubjectCreatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2214, 715);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.subjectsGridView);
+            this.Name = "SubjectCreatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wprowadzanie przedmiotów";
+            ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView subjectsGridView;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
