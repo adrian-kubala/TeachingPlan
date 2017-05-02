6b065bc0b12511f0bce4b890874224b1f24cc1fa
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-02 15:41:12 +0200

    Add columns

--- TeachingPlan/SubjectCreatorForm.Designer.cs
+++ TeachingPlan/SubjectCreatorForm.Designer.cs
@@ -31,6 +31,16 @@
             this.subjectsGridView = new System.Windows.Forms.DataGridView();
             this.exitButton = new System.Windows.Forms.Button();
             this.saveButton = new System.Windows.Forms.Button();
+            this.subjectName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studyName = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.studyMode = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.teacher = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.speciality = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.deanGroup = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.hoursAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.semesterNumber = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.classesMode = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.ectsPoints = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +49,19 @@
             this.subjectsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.subjectsGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.subjectsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.subjectsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.subjectName,
+            this.studyName,
+            this.studyMode,
+            this.teacher,
+            this.speciality,
+            this.deanGroup,
+            this.hoursAmount,
+            this.semesterNumber,
+            this.classesMode,
+            this.ectsPoints});
             this.subjectsGridView.Location = new System.Drawing.Point(30, 30);
             this.subjectsGridView.Name = "subjectsGridView";
             this.subjectsGridView.RowTemplate.Height = 33;
@@ -68,6 +90,67 @@
             this.saveButton.Text = "Zapisz i wyjdź";
             this.saveButton.UseVisualStyleBackColor = true;
             // 
+            // subjectName
+            // 
+            this.subjectName.HeaderText = "Nazwa_przedmiotu";
+            this.subjectName.Name = "subjectName";
+            this.subjectName.Width = 240;
+            // 
+            // studyName
+            // 
+            this.studyName.HeaderText = "Studia";
+            this.studyName.Name = "studyName";
+            this.studyName.Width = 79;
+            // 
+            // studyMode
+            // 
+            this.studyMode.HeaderText = "Tryb_studiow";
+            this.studyMode.Name = "studyMode";
+            this.studyMode.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.studyMode.Width = 146;
+            // 
+            // teacher
+            // 
+            this.teacher.HeaderText = "Wykladowca";
+            this.teacher.Name = "teacher";
+            this.teacher.Width = 139;
+            // 
+            // speciality
+            // 
+            this.speciality.HeaderText = "Specjalnosc";
+            this.speciality.Name = "speciality";
+            this.speciality.Width = 135;
+            // 
+            // deanGroup
+            // 
+            this.deanGroup.HeaderText = "Grupa_dziekanska";
+            this.deanGroup.Name = "deanGroup";
+            this.deanGroup.Width = 198;
+            // 
+            // hoursAmount
+            // 
+            this.hoursAmount.HeaderText = "Ilosc_godzin";
+            this.hoursAmount.Name = "hoursAmount";
+            this.hoursAmount.Width = 177;
+            // 
+            // semesterNumber
+            // 
+            this.semesterNumber.HeaderText = "Numer_semestru";
+            this.semesterNumber.Name = "semesterNumber";
+            this.semesterNumber.Width = 181;
+            // 
+            // classesMode
+            // 
+            this.classesMode.HeaderText = "Tryb_zajec";
+            this.classesMode.Name = "classesMode";
+            this.classesMode.Width = 124;
+            // 
+            // ectsPoints
+            // 
+            this.ectsPoints.HeaderText = "Punkty_ECTS";
+            this.ectsPoints.Name = "ectsPoints";
+            this.ectsPoints.Width = 191;
+            // 
             // SubjectCreatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
@@ -79,6 +162,7 @@
             this.Name = "SubjectCreatorForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Wprowadzanie przedmiotów";
+            this.Load += new System.EventHandler(this.SubjectCreatorForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).EndInit();
             this.ResumeLayout(false);
 
@@ -89,5 +173,15 @@
         private System.Windows.Forms.DataGridView subjectsGridView;
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subjectName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn studyName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn studyMode;
+        private System.Windows.Forms.DataGridViewComboBoxColumn teacher;
+        private System.Windows.Forms.DataGridViewComboBoxColumn speciality;
+        private System.Windows.Forms.DataGridViewComboBoxColumn deanGroup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hoursAmount;
+        private System.Windows.Forms.DataGridViewComboBoxColumn semesterNumber;
+        private System.Windows.Forms.DataGridViewComboBoxColumn classesMode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ectsPoints;
     }
 }
--- TeachingPlan/SubjectCreatorForm.cs
+++ TeachingPlan/SubjectCreatorForm.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,37 @@ namespace TeachingPlan
         public SubjectCreatorForm()
         {
             InitializeComponent();
+        }
+
+        private void SubjectCreatorForm_Load(object sender, EventArgs e)
+        {
+            //DataTable table = new DataTable();
+
+            //using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.teachingPlanConnectionString))
+            //{
+            //    SqlCommand teachingPlanCommand = new SqlCommand("SELECT Nazwa_studiow as Studia FROM Studia;", connection);
+            //    try
+            //    {
+            //        SqlDataAdapter dataAdapter = new SqlDataAdapter(teachingPlanCommand);
+
+            //        table = new DataTable();
+            //        dataAdapter.Fill(table);
+            //    }
+            //    catch (Exception exeption)
+            //    {
+            //        MessageBox.Show(exeption.Message);
+            //    }
+            //}
+
+            //DataGridViewTextBoxColumn colum = new DataGridViewTextBoxColumn();
+            //colum.Name = "Nazwa przedmiotu";
+            //subjectsGridView.Columns.Add(colum);
+
+            //DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+            //comboBoxColumn.DataSource = table;
+            //comboBoxColumn.DisplayMember = "Studia";
+            //comboBoxColumn.Name = comboBoxColumn.DisplayMember.ToString();
+            //subjectsGridView.Columns.Add(comboBoxColumn);
         }
     }
 }
